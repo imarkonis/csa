bc7089aadd293ca08a72b721151471878b8158a5
Mijael Vargas <mirovago@gmail.com>
2020-03-24 17:31:26 +0100

    Package dev check
    Minnor changes on syntax to remove  as many errors and warnings as possible

--- R/graphics.R
+++ R/graphics.R
@@ -13,13 +13,13 @@
 #' @export
 #' @examples
 #' aa <- rnorm(1000)
-#' csa_aa <- csa(aa, plot = F)
+#' csa_aa <- csa(aa, plot = FALSE)
 #' csa.plot(csa_aa)
 
-csa.plot <- function(x, log_x = T, log_y = T, smooth = F, wn = F){
+csa.plot <- function(x, log_x = TRUE, log_y = TRUE, smooth = FALSE, wn = FALSE){
   colnames(x) <- c("scale", "Value")
   df <- as.data.frame(x)
-  if(smooth == F){
+  if(smooth == FALSE){
     gp <- ggplot(data = df, aes_string(x = "scale", y = "Value")) +
       geom_line(size = 0.5) +
       geom_point() +
@@ -29,30 +29,30 @@ csa.plot <- function(x, log_x = T, log_y = T, smooth = F, wn = F){
   else{
     gp <- ggplot(data = df, aes_string(x = "scale", y = "Value")) +
       geom_line(size = 1,
-                stat = 'smooth', method = "loess", se = F, span = 1) +
+                stat = 'smooth', method = "loess", se = FALSE, span = 1) +
       theme_bw() +
       theme(panel.grid.minor.x = element_blank(),
             panel.grid.minor.y = element_blank())}
-  if(log_x == T){
+  if(log_x == TRUE){
     gpp <- gp + scale_x_log10("Aggregation scale [-]",
                               labels = trans_format("log10", math_format(10 ^ .x)),
                               breaks = trans_breaks("log10",
-                                                    n = abs(round(log10(min(df[, 2], na.rm = T)))) + 1,
+                                                    n = abs(round(log10(min(df[, 2], na.rm = TRUE)))) + 1,
                                                     function(x) 10 ^ x)) +
       annotation_logticks(sides = "b")
   } else {
     gpp <- gp + scale_x_continuous("Aggregation scale [-]")
   }
-  if(log_y == T){
+  if(log_y == TRUE){
     gppp <- gpp + scale_y_log10("Value", labels = trans_format("log10", math_format(10^.x)),
                                 breaks = trans_breaks("log10",
-                                                      n = abs(round(log10(min(df[, 2], na.rm = T)))) + 1,
+                                                      n = abs(round(log10(min(df[, 2], na.rm = TRUE)))) + 1,
                                                       function(x) 10 ^ x)) +
       annotation_logticks(sides = "b")
   } else {
     gppp <- gpp + scale_y_continuous("Value")
   }
-  if(wn == T){
+  if(wn == TRUE){
     gppp <- gppp + geom_abline(slope = -1, size = 1, col = "dark gray", linetype = "dashed")
   }
   gppp
@@ -62,7 +62,7 @@ csa.plot <- function(x, log_x = T, log_y = T, smooth = F, wn = F){
 #'
 #' Function for plotting multiple CSA curves in a single plot.
 #'
-#' @param x A matrix or data.frame composed of three columns;
+#' @param df A matrix or data.frame composed of three columns;
 #' scale for the temporal or spatial scale; value for the estimate of a given statistic (e.g., variance) at the given aggregated scale and
 #' variable for defining the corresponding dataset.
 #' @param log_x logical. If TRUE (the default) the x axis of the CSA plot is set to the logarithmic scale.
@@ -74,13 +74,13 @@ csa.plot <- function(x, log_x = T, log_y = T, smooth = F, wn = F){
 #' @export
 #' @examples
 #' aa <- rnorm(1000)
-#' csa_aa <- data.frame(csa(aa, plot = F), variable = 'wn')
+#' csa_aa <- data.frame(csa(aa, plot = FALSE), variable = 'wn')
 #' bb <- as.numeric(arima.sim(n = 1000, list(ar = c(0.8897, -0.4858), ma = c(-0.2279, 0.2488))))
-#' csa_bb <- data.frame(csa(bb, plot = F), variable = 'arma(2, 2)')
-#' csa.multiplot(rbind(csa_aa, csa_bb), wn = T)
-#' csa.multiplot(rbind(csa_aa, csa_bb), wn = T, smooth = T)
+#' csa_bb <- data.frame(csa(bb, plot = FALSE), variable = 'arma(2, 2)')
+#' csa.multiplot(rbind(csa_aa, csa_bb), wn = TRUE)
+#' csa.multiplot(rbind(csa_aa, csa_bb), wn = TRUE, smooth = TRUE)
 
-csa.multiplot <- function(df, log_x = T, log_y = T, wn = F, smooth = F){
+csa.multiplot <- function(df, log_x = TRUE, log_y = TRUE, wn = FALSE, smooth = FALSE){
   colnames(df) <- c("scale", "value", "variable")
   df <- as.data.frame(df)
   no_var <- length(unique(df$variable))
@@ -92,7 +92,7 @@ csa.multiplot <- function(df, log_x = T, log_y = T, wn = F, smooth = F){
     cols <- c("#807dba", "#74add1", "#78c679", "#fdae61", "#f46d43",
               "#d73027", "#fee090", "#d9f0a3", "#abd9e9", "#4575b4")[1:no_var]
   }
-  if(smooth == F){
+  if(smooth == FALSE){
     gp <- ggplot(data = df, aes_string(x = df[ ,1], y = df[ ,2])) +
       geom_line(aes(group = interaction(variable),
                     colour = factor(variable)), size = 0.5, alpha = transp) +
@@ -107,7 +107,7 @@ csa.multiplot <- function(df, log_x = T, log_y = T, wn = F, smooth = F){
     gp <- ggplot(data = df, aes_string(x = df[ ,1], y = df[ ,2])) +
       geom_line(aes(group = interaction(variable),
                     colour = factor(variable)),
-                stat='smooth', method = "loess", se = F, span = 1,
+                stat='smooth', method = "loess", se = FALSE, span = 1,
                 size = 1, alpha = transp) +
       scale_colour_manual("", values = cols) +
       theme_bw() +
@@ -115,26 +115,26 @@ csa.multiplot <- function(df, log_x = T, log_y = T, wn = F, smooth = F){
             panel.grid.minor.y = element_blank())
   }
   if(no_var > 10){gp <- gp + rremove("legend")}
-  if(log_x == T){
+  if(log_x == TRUE){
     gpp <- gp + scale_x_log10("Aggregation scale [-]",
                               labels = trans_format("log10", math_format(10 ^ .x)),
                               breaks = trans_breaks("log10",
-                                                    n = abs(round(log10(min(df[, 2], na.rm = T)))) + 1,
+                                                    n = abs(round(log10(min(df[, 2], na.rm = TRUE)))) + 1,
                                                     function(x) 10 ^ x)) +
       annotation_logticks(sides = "b")
   } else {
     gpp <- gp + scale_x_continuous("Aggregation scale [-]")
   }
-  if(log_y == T){
+  if(log_y == TRUE){
     gppp <- gpp + scale_y_log10("Value", labels = trans_format("log10", math_format(10^.x)),
                                 breaks = trans_breaks("log10",
-                                                      n = abs(round(log10(min(df[, 2], na.rm = T)))) + 1,
+                                                      n = abs(round(log10(min(df[, 2], na.rm = TRUE)))) + 1,
                                                       function(x) 10 ^ x)) +
       annotation_logticks(sides = "b")
   } else {
     gppp <- gpp + scale_y_continuous("Value")
   }
-  if(wn == T){
+  if(wn == TRUE){
     gppp <- gppp + geom_abline(slope = -1, size = 1, col = "dark gray", linetype = "dashed")
   }
   gppp
